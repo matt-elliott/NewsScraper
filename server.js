1cc967e7c81aa66c972fc8c9c89dba5914f7e016
Matt Elliott <mattelliottus@gmail.com>
2019-08-08 18:46:37 -0700

    created root path variable

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 const config = require('./config/config.json');
 const environemnt = process.env.NODE_ENV || 'development';
 const environemntConfig = config[environemnt];
+__rooutpath = __dirname;
 
-require('./routes/views')(app);
+require('./routes/views')(app, __rooutpath);
 
 app.listen(environemntConfig.PORT, function () {
   console.log('\nApp Live On : ', environemntConfig.PORT, '\nVVVVVVVV\n\n');
